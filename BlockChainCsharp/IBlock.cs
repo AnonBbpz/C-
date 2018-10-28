df4a23476af6697fca567fc27c8c62406486d02f
AnonBbpz <42625495+AnonBbpz@users.noreply.github.com>
2018-10-28 19:38:35 -0400

    Create IBlock.cs

---
+++ BlockChainCsharp/IBlock.cs
@@ -0,0 +1,25 @@
+using System;
+
+
+
+namespace nl.hyperdata.blockchain
+
+{
+
+    public interface IBlock
+
+    {
+
+        byte[] Data { get; }
+
+        byte[] Hash { get; set; }
+
+        int Nonce { get; set;}
+
+        byte[] PreviousHash { get; set;}
+
+        DateTime TimeStamp { get; }
+
+    }
+
+}
